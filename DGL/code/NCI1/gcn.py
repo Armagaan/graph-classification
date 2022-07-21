f54af9ac0e0f2bd4f2720931a00025343e6f88e7
Burooj Armagaan <armgaan009@gmail.com>
2022-07-21 18:40:21 +0530

    - Input to gcn now identical to cfsqr.

--- DGL/code/NCI1/gcn.py
+++ DGL/code/NCI1/gcn.py
@@ -71,12 +71,15 @@ class GCNGraph(torch.nn.Module):
         self.dense3 = torch.nn.Linear(8, 1)
 
     def forward(self, g, in_feat, e_weight):
-        dense_adj = torch.sparse.FloatTensor(e_weight, torch.ones(e_weight.size(1)))
-        h = self.conv1(in_feat, dense_adj)
+        mat_size = int(math.sqrt(e_weight.size(0)))
+        dense = e_weight.reshape(mat_size, mat_size)
+        sparse_adj = dense.to_sparse()
+
+        h = self.conv1(in_feat, sparse_adj)
         h = torch.nn.functional.relu(h)
-        h = self.conv2(h, dense_adj)
+        h = self.conv2(h, sparse_adj)
         h = torch.nn.functional.relu(h)
-        h = self.conv3(h, dense_adj)
+        h = self.conv3(h, sparse_adj)
         g.ndata['h'] = h
         h = dgl.readout_nodes(g, 'h', op='mean') # pooling
         h = self.dense1(h)
@@ -86,6 +89,7 @@ class GCNGraph(torch.nn.Module):
         h = self.dense3(h)
         h = torch.sigmoid(h)
         return h
+
 
 
 class GCNNodeBAShapes(torch.nn.Module):
